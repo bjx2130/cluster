603466a412fdbe48a91cc42e1196aba2ad395093
bjx <bjx@ubuntu.xa.hoperun.com>
2017-12-26 13:40:07 +0800

    提交启动脚本

--- eureka/Eureka-Server-Cluster/EurekaClusterStart.sh
+++ eureka/Eureka-Server-Cluster/EurekaClusterStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#伪集群启动脚本
+#伪集群启动脚本......
 
 nohup java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=A & #端口：1001
 nohup java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=B & #端口：1002
