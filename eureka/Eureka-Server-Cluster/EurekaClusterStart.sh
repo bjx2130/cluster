629722bdbbf1965c9f1e8dc92ce2eb3eae40f946
bjx2130 <308110158@qq.com>
2017-12-26 13:29:18 +0800

    Update EurekaClusterStart.sh

--- eureka/Eureka-Server-Cluster/EurekaClusterStart.sh
+++ eureka/Eureka-Server-Cluster/EurekaClusterStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+#伪集群启动脚本
 
-
-nohup sudo java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=A & #端口：1001
-nohup sudo java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=B & #端口：1002
+nohup java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=A & #端口：1001
+nohup java -jar Eureka-Server-0.0.1-SNAPSHOT.jar --spring.profiles.active=B & #端口：1002
 
